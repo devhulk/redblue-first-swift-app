d70ce018ef1b77a3f57ef3ff5110ef2a5496b33d
Gerald Yerden <geraldyerden@Gerald-Yerdens-MacBook-Pro.local>
2016-05-21 20:24:10 -0400

    Addid files for bluered

--- bluered/ViewController.swift
+++ bluered/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var RedHat: UIImageView!
+    @IBOutlet weak var BlueHat: UIImageView!
+    @IBOutlet weak var RedHatButton: UIButton!
+    @IBOutlet weak var BlueHatButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +23,14 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func redHatDissapear(sender: AnyObject) {
+        RedHat.hidden = true
+    }
+    
+    @IBAction func blueHatDissapear(sender: AnyObject) {
+        BlueHat.hidden = true
     }
 
 
